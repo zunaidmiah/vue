b0c6059bae0bf622a5848961726de8329d4f4066
Zunaid Miah <45538749+zunaidmiah@users.noreply.github.com>
2024-01-31 01:11:37 +0600

    load vue file for app

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 // import App from './App.vue'
 // import App from './App1.vue'
 // import App from './AppHttp.vue'
-import App from './AppMixins.vue'
+// import App from './AppMixins.vue'
+import App from './computed.vue'
 
 import './assets/main.css'
 
