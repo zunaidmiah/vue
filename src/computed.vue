9f9648d2d15eff784fb99bb1ecbaafde22f58de2
Zunaid Miah <45538749+zunaidmiah@users.noreply.github.com>
2024-01-31 01:11:15 +0600

    add composition api vue file

---
+++ src/computed.vue
@@ -0,0 +1,71 @@
+<template>
+    <Header />
+    <div class="container">
+        <!-- Options API -->
+        <input type="text" placeholder="First Name" v-model="fName" />
+        <input type="text" placeholder="Last Name" v-model="lName" />
+        <h2>Options Fullname is {{ fullName }}</h2>
+
+        <!-- ref -->
+        <input type="text" placeholder="First Name" v-model="refFirstName" />
+        <input type="text" placeholder="Last Name" v-model="refLastName" />
+        <h2>Ref Fullname is {{ refFullName }}</h2>
+
+        <!-- reactive -->
+        <input type="text" placeholder="First Name" v-model="reactiveFirstName" />
+        <input type="text" placeholder="Last Name" v-model="reactiveLastName" />
+        <h2>Reactive Fullname is {{ reactiveFullName }}</h2>
+    </div>
+    <Footer />
+</template>
+
+<script>
+import Header from './Components/main/header.vue';
+import Footer from './Components/main/footer.vue';
+import { ref, reactive, toRefs, computed } from 'vue'
+export default {
+    name: 'Computed',
+    setup() {
+        const refFirstName = ref('')
+        const refLastName = ref('')
+
+        const state = reactive({
+            reactiveFirstName: '',
+            reactiveLastName: '',
+        })
+
+        const refFullName = computed(function () {
+            return `${refFirstName.value} ${refLastName.value}`
+        })
+
+        const reactiveFullName = computed(function () {
+            return `${state.reactiveFirstName} ${state.reactiveLastName}`
+        })
+
+        return {
+            refFirstName,
+            refLastName,
+            refFullName,
+            ...toRefs(state),
+            reactiveFullName,
+        }
+    },
+    data() {
+        return {
+            fName: '',
+            lName: '',
+        }
+    },
+    computed: {
+        fullName() {
+            return `${this.fName} ${this.lName}`
+        },
+    },
+    components: {
+        Header,
+        Footer
+    }
+}
+</script>
+
+<style scoped></style>
